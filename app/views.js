7d803b7671bd0f2e8777288d1a64dba2a799cf73
petfle <peter.tollnes.flem@knowit.no>
2014-05-19 13:51:37 +0200

    Deleted/moved files during the restructure

--- app/views.js
+++
@@ -1,8 +0,0 @@
-module.exports.render = function(response, view, title, viewData) {
-    response.render(view, viewData || {}, function (err, html) {
-        response.render('shared/root', {
-            body: html,
-            title: title || view
-        });
-    });
-}
