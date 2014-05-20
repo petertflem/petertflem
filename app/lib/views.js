68a91faec39630670174355dae0e77374c8fdf1d
petfle <peter.tollnes.flem@knowit.no>
2014-05-20 15:05:52 +0200

    Cleaned views.js up a little

--- app/lib/views.js
+++ app/lib/views.js
@@ -1,7 +1,3 @@
 module.exports.render = function(response, view, req, viewData) {
-    response.render(view, viewData || {}, function (err, html) {
-        req.xhr 
-            ? response.end(html) 
-            : response.render('layout/layout');
-    });
+    req.xhr ? response.render(view, viewData || {}) : response.render('layout/layout')
 }
